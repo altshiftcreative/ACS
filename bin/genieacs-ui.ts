becd97c7fa7768644edc8bf3f5eb0ef0c169f638
Sanad Amin <sanad@altshiftcreative.com>
2020-12-13 16:47:41 +0200

    fixed cookie issue

--- bin/genieacs-ui.ts
+++ bin/genieacs-ui.ts
@@ -105,16 +105,19 @@ if (!cluster.worker) {
     if (stopping) {
       res.setHeader("Connection", "close");
     }
-    
-    res.setHeader("Access-Control-Allow-Origin","*");
+    res.setHeader("Access-Control-Allow-Origin","http://localhost:4200");
     res.setHeader("Access-Control-Allow-Methods","GET, POST, PATCH, PUT, DELETE, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers","Origin, Content-Type, X-Auth-Token");
+    res.setHeader("Access-Control-Allow-Credentials","true");
+    res.setHeader("Access-Control-Expose-Headers","Set-Cookie");
+
+    
     if (req.method === "OPTIONS") {
       console.log("Options",req.headers)
       res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
       res.writeHead(200)
     } else {
-      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Origin', "http://localhost:4200");
     }
   
     listener(req, res);
